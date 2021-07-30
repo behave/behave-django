28eecd98ee6ee7db79c7c309e00110351e8597b2
rodrigondec <rodrigondec@gmail.com>
2021-07-30 15:24:28 -0300

    add steps

--- tests/acceptance/steps/running_tests.py
+++ tests/acceptance/steps/running_tests.py
@@ -1,5 +1,7 @@
 from behave import given, when, then
 
+# from tests.test_app.tests import MyCustomTestCase
+
 
 @given(u'this step exists')
 def step_exists(context):
@@ -16,10 +18,17 @@ def is_running(context):
     pass
 
 
+@then(u'the test_runner should be MyCustomTestRunner')
+def get_runner_dynamically(context):
+    # assert context.test_runner.is_custom
+    pass
+
 @then(u'before_django_ready should be called')
 def before_django_context(context):
     assert context.before_django
     assert context.test_runner.before_django
+    # assert MyCustomTestCase in context.test_runner.testcase_class.mro()
+    # assert context.test_runner.testcase_class.is_custom
 
 
 @then(u'django_ready should be called')
