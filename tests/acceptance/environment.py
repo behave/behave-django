4017c2f618c1222dce58c8a18ea9ebf80de79d80
Peter Bittner <django@bittner.it>
2025-11-15 14:35:55 +0100

    Fix fixtures not being reset between features when set to empty list

--- tests/acceptance/environment.py
+++ tests/acceptance/environment.py
@@ -11,6 +11,12 @@ def before_feature(context, feature):
         # Including empty fixture to test that #92 is fixed
         context.fixtures = ['empty-fixture.json']
 
+    elif feature.name == 'Fixture reset - first feature with fixtures':
+        context.fixtures = ['behave-fixtures.json']
+
+    elif feature.name == 'Fixture reset - second feature with empty fixtures':
+        context.fixtures = []
+
 
 def before_scenario(context, scenario):
     if scenario.name == 'Load fixtures for this scenario and feature':
---
+++ tests/acceptance/features/fixture-reset-first.feature
@@ -0,0 +1,8 @@
+@requires-live-http
+Feature: Fixture reset - first feature with fixtures
+    In order to test fixture reset between features
+    As a developer
+    I want to load fixtures in the first feature
+
+    Scenario: First feature loads fixtures
+        Then the fixture should be loaded
